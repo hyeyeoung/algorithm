f5fcf6bc1a379a9a5095f098b438a3a7cdb23b60
hyeyeoung <81437055+hyeyeoung@users.noreply.github.com>
2022-08-01 16:14:31 +0900

    [Bronze I] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int i = 1;
+
+	while (n > i) {
+		n -= i;
+		i++;
+	}
+	if (i % 2 == 1) {
+		cout << i + 1 - n << '/' << n;
+	}
+	else {
+		cout << n << '/' << i + 1 - n;
+	}
+}
