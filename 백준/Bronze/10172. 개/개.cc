cb17616313b0055575410b88671e6946de9d08f5
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-05-29 22:43:07 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,13 @@
+// #define _CRT_SECURE_NO_DEPRECATE
+// #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// #include <string>
+using namespace std;
+
+int main() {
+	cout << "|\\_\/|"<<endl;
+	cout << "|q p|   \/}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"\`    |" << endl;
+	cout << "||_\/=\\\\__|" << endl;
+}
