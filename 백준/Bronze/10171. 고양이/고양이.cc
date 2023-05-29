5f2cee1ef93e727158f9af344a16d733727a0759
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-05-29 22:39:46 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+// #define _CRT_SECURE_NO_DEPRECATE
+// #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// #include <string>
+using namespace std;
+
+int main() {
+	cout << "\\    \/\\"<<endl;
+	cout << " )  ( \')" << endl;
+	cout << "(  \/  )" << endl;
+	cout << " \\(__)|" << endl;
+
+}
