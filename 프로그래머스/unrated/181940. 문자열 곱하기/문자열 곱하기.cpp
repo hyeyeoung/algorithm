60a13613cf23e5cff1e5829a8860bd7b6252a3e1
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-07-13 13:04:00 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int k) {
+    string answer = "";
+    while(k--){
+        answer += my_string;
+    }
+    return answer;
+}
