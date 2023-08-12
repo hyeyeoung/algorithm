b5b0996c4830b62e0809b113b516396554139c12
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-08-13 03:08:48 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -1,34 +1,34 @@
+/*  도키도키 간식드리미  */
+
 #include <iostream>
 #include <stack>
 using namespace std;
+void init() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+}
 
 int main() {
-    int n;
-    cin >> n;
-    
-    stack<int> st;
-    int expected = 1;  // 기대되는 다음 숫자
-    
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        
-        if (num == expected) {
-            expected++;
-            while (!st.empty() && st.top() == expected) {
-                st.pop();
-                expected++;
-            }
-        } else {
-            st.push(num);
-        }
-    }
-    
-    if (st.empty()) {
-        cout << "Nice";
-    } else {
-        cout << "Sad";
-    }
-    
-    return 0;
+	init();
+	int n;
+	cin >> n;
+	int i = 1;
+	stack <int> st;
+	while (n--) {
+		int num;
+		cin >> num;
+
+		if (i == num) {
+			i++;
+			while (!st.empty() && st.top() == i) {
+				st.pop();
+				i++;
+			}
+		}
+		else st.push(num);
+	}
+
+	if (st.empty()) cout << "Nice";
+	else cout<< "Sad";
 }
