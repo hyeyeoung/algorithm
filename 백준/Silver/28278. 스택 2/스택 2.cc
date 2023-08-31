a7129cc278bb0ab2a3fc0636eed4abea5bb847b9
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-08-31 15:35:07 +0900

    [Silver IV] Title: 스택 2, Time: 144 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.cc
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -1,16 +1,43 @@
 #include <iostream>
-#include <stack>
-
 using namespace std;
+class stack {
+private:
+	int Stack[1000001];
+	int top = 0;
+public:
+	void push(int num);
+	int pop();
+	int size();
+	int isEmpty();
+	int Top();
+};
 
-void init() {
+void stack::push(int num) {
+	Stack[top] = num;
+	top++;
+}
+int stack::pop() {
+	int tmp;
+	if (top) tmp = Stack[--top];
+	else tmp = -1;
+	return tmp;
+}
+int stack::size() {
+	return top;
+}
+int stack::isEmpty() {
+	if (top) return 0;
+	else return 1;
+}
+int stack::Top() {
+	if (top) return Stack[top-1];
+	else return -1;
+}
+int main() {
 	cin.tie(0);
 	cout.tie(0);
 	ios_base::sync_with_stdio(false);
-}
-int main() {
-	init();
-	stack<int> x;
+	stack stack;
 	int n;
 	cin >> n;
 
@@ -20,17 +47,11 @@ int main() {
 		if (i1 == 1) {
 			int i2;
 			cin >> i2;
-			x.push(i2);
+			stack.push(i2);
 		}
-		else if (i1 == 2) {
-			if (x.empty()) cout << -1 << '\n';
-			else { cout << x.top() << '\n'; x.pop(); }
-		}
-		else if (i1 == 3) { cout << x.size() << '\n'; }
-		else if (i1 == 4) { cout << x.empty() << '\n'; }
-		else if (i1 == 5) {
-			if (x.empty()) cout << -1 << '\n';
-			else cout << x.top() << '\n';
-		}
+		else if (i1 == 2) cout<<stack.pop()<<"\n";
+		else if (i1 == 3) cout << stack.size() << "\n";
+		else if (i1 == 4) cout << stack.isEmpty() << "\n";
+		else if (i1 == 5) cout << stack.Top() << "\n";
 	}
 }
