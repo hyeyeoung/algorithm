b3ee6924ad21dcb598f8ba2060fa8454397e2001
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-09-19 14:59:12 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int tile(int n) {
+	int arr[100001];
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i <= n; i++){
+		arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+	}
+	return arr[n];
+}
+int main() {
+	int N;
+	cin >> N;
+	cout << tile(N);
+}
