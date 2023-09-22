29cde1e978a0c6ce45c5e63771cf661ad6907af2
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-09-22 15:20:21 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, x, a;
+	scanf("%d %d", &n, &x);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+
+		if (x > a)
+			printf("%d ", a);
+	}
+		return 0;
+}
