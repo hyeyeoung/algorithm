d48b73055f1f89322fe96696ec16f328454062d5
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-11-28 16:48:53 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,26 +1,45 @@
+/* Brute Force 풀이 */
+
 #include <iostream>
 using namespace std;
-void init() { cin.tie(0); cout.tie(0); ios::sync_with_stdio(false); }
-int arr[500];
-int dp(int n) {
-	arr[1] = 1;
-	arr[2] = 2;
-	arr[3] = 4;
-	arr[4] = 7;
-	for (int i = 5; i <= n; i++) {
-		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-	}
-	return arr[n];
-}
 
-int main() {
-	init();
+int main(){
+    int TC;
+    cin>>TC;
 
-	int T;
-	cin >> T;
-	while (T--) {
-		int n;
-		cin >> n;
-		cout << dp(n)<<'\n';
-	}
-}
+    while(TC--){
+        int n; cin>>n;
+        int ans = 0;
+        for(int l1=1; l1<=3; l1++){
+            if(l1 == n) ans +=1;
+            for(int l2 = 1; l2 <= 3; l2++){
+                if(l1 + l2 == n) ans +=1;
+                for(int l3=1; l3<=3; l3++){
+                    if(l1 + l2 + l3 == n) ans +=1;
+                    for(int l4 = 1; l4 <= 3; l4++){
+                        if(l1 + l2 +l3 + l4 == n) ans +=1;
+                        for(int l5 = 1; l5 <=3 ; l5++){
+                            if(l1 + l2 + l3 + l4 + l5 == n) ans += 1;
+                            for(int l6 = 1; l6 <= 3; l6++){
+                                if(l1 + l2 + l3 + l4 + l5 + l6 == n )ans += 1;
+                                for(int l7 = 1; l7 <=3; l7++){
+                                 if(l1 + l2 + l3 + l4 + l5 + l6 + l7 == n )ans += 1;
+                                    for(int l8 = 1; l8 <=3; l8++){
+                                        if(l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8== n )ans += 1;
+                                        for(int l9 = 1; l9 <=3; l9++){
+                                            if(l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 == n )ans += 1;
+                                            for(int l10 = 1; l10 <=3; l10++){
+                                                if(l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 == n )ans += 1;
+                                            }   
+                                        }
+                                    }   
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cout << ans <<'\n';
+    } //while
+} // main
