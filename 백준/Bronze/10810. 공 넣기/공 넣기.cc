f1c892bf4102c70b58b4d6613ef7ad20075ad3f8
hy <81437055+hyeyeoung@users.noreply.github.com>
2023-12-20 16:05:05 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    int arr[101] ={};
+
+    for(int i = 1; i<=m;i++){
+        int a,b,c;
+        cin>> a>>b>>c;
+        for(int j = a; j<=b; j++){
+            arr[j] = c;
+        }
+    }
+
+    for(int i = 1; i<=n; i++) cout << arr[i]<<" ";
+}
