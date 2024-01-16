aa52f92d9c8d7eb9e78195e8a8c97a8f4985aedd
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-01-17 00:18:36 +0900

    [Silver III] Title: 다음 순열, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -1,41 +1,39 @@
 #include <iostream>
 using namespace std;
-int n_permitation[10001] = {0};
+int arr[10001] = {};
 
-void n_p (int n){
+void next_permutation(int n){
     int index_i = 0;
-    for(int i = 0; i < n; i++){ // i 찾기
-        if(n_permitation[i] > n_permitation[i-1] && index_i <= i) index_i = i;
+    for(int i = 0; i<n; i++){
+        if (arr[i-1] < arr[i] && index_i <= i) index_i = i;
     }
-    // while(index_i > 0 && n_permitation[index_i-1] <= n_permitation[index_i]) index_i--;
-    if(!index_i) cout << -1; 
+    if(!index_i) cout << -1;
     else{
         int index_j = 0;
-        for(int j = 0; j < n; j++){ // j 찾기
-        if(j >= index_i && n_permitation[j] > n_permitation[index_i-1] && index_j <= j) index_j = j;
+        for(int j = index_i; j<n; j++){
+           if(arr[j] > arr[index_i - 1] && index_j <= j) index_j = j;
         }
-
-        int tmp = n_permitation[index_i-1];
-        n_permitation[index_i-1] = n_permitation[index_j];
-        n_permitation[index_j] = tmp;
-        // cout << index_i << index_j <<"\n";
+    // swap 
+        int tmp = arr[index_i - 1];
+        arr[index_i-1] = arr[index_j];
+        arr[index_j] = tmp;
         index_j = n-1;
         while(index_i < index_j){
-            int tmp_a = n_permitation[index_i];
-            n_permitation[index_i] = n_permitation[index_j];
-            n_permitation[index_j] = tmp_a;
+            int tmp1 = arr[index_i];
+            arr[index_i] = arr[index_j];
+            arr[index_j] = tmp1;
             index_i++; index_j--;
         }
-        for(int i =0 ;i<n;i++){
-            cout << n_permitation[i]<<" ";
-        }
+        for(int i = 0; i<n; i++){
+        cout << arr[i]<<" ";
+    }
     }
 }
-
-
 int main(){
-    int n; cin >> n;
+    int n; 
+    cin >> n;
+    for(int i = 0; i<n; i++){ cin >> arr[i]; }
 
-    for(int i = 0; i<n; i++) cin >> n_permitation[i];
-    n_p(n);
+    next_permutation(n);
+
 }
