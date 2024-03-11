8c2ffc2940cbe0bcad6599b72812567b0f47f736
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-11 21:25:22 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[10]; int a[10]; int b[10] = {0};
+void go(int index,int start, int n, int m){
+    if(index == m+1){
+        for(int i = 1; i<=m; i++) cout <<arr[a[i]]<<" ";
+        cout <<"\n";
+        return;
+    }
+    for(int i = start; i<=n; i++){
+        if(b[i] > 0){
+            b[i]-=1;
+            a[index] = i;
+            go(index+1, i, n, m);
+            b[i]+=1;
+        }
+    }
+}
+
+int main(){
+    int n, m; cin >> n >> m;
+    for(int i = 1; i<=n;i++) cin >> arr[i];
+    sort(arr,arr+1+n);
+    
+    for(int i = 1; i<=n; i+=b[i]){
+        int count = 1;
+        for(int j = i+1; j<=n; j++){
+            if(arr[i] == arr[j]) count++;
+            else break;
+        }
+        b[i] = count;
+    }
+    go(1,1, n,m);
+}
