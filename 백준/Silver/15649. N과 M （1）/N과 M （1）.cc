63f337679caa8f73d5bffe88d2bb97d7b89eabce
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-13 20:44:21 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
-int a[10] = {0};
-bool b[10] = {0};
-void go(int index, int n, int m){
+int arr[10] = {};
+bool b[10] = {};
+int n, m; 
+void go(int index){
     if(index == m+1){
-        for(int i = 1; i <= m; i++) {
-            cout << a[i] <<" ";
-        }
+        for(int i = 1; i<=m; i++) cout << arr[i] <<" ";
         cout <<'\n';
         return;
     }
-    for(int i = 1; i<=n;i++){
+
+    for(int i = 1; i<=n; i++){
         if(b[i]) continue;
-        b[i] = true; a[index] = i;
-        go(index+1, n, m);
+        b[i] = true; arr[index] = i;
+        go(index + 1);
         b[i] = false;
     }
 }
 
 int main(){
-    int m, n; cin >> n >> m;
-    go(1,n,m);
+    cin >> n >> m;
+    go(1);
 }
