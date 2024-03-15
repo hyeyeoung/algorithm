a0a7096702ce867667c8b509e217ad9824c42f28
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-15 18:32:13 +0900

    [Gold IV] Title: 이분 그래프, Time: 744 ms, Memory: 11300 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,45 +1,44 @@
+// bfs로 재도전
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <queue>
 using namespace std;
-vector <int> arr[20001];
+vector <int> g[20001];
 int color[20001] = {};
-
-void dfs(int x, int col){
-    color[x] = col;
-    for(int i = 0; i<arr[x].size(); i++){
-        int next = arr[x][i];
-        if(color[next] == 0){
-            dfs(next, 3 - col);
+bool bfs(int node, int c){
+    color[node] = c;
+    for(int i =0; i<g[node].size(); i++){
+        int next = g[node][i];
+        if(color[next]==0){
+            if(bfs(next, 3 -c) == false) return false;
         }
+        else if(color[next] == color[node]) return false;
     }
+    return true;
 }
 
 int main(){
-    int t; cin >> t;
-    while(t--){
-        int v, e; cin >> v >> e;
-        for(int i = 1; i<=v; i++){
+    int tc; cin >> tc;
+    while(tc--){
+        int v, e; // v = vertex개수, e = edge 개수
+        cin >> v >> e;
+        // 초기화 과정, 하나의 arr, vector를 여러번 재활용하기 때문
+        for(int i = 1; i<= v; i++){ 
             color[i] = 0;
-            arr[i].clear();
+            g[i].clear(); 
         }
-        for(int i = 0; i<e; i++) {
+        for(int i = 0; i<e; i++){
             int a, b; cin >> a >> b;
-            arr[a].push_back(b); arr[b].push_back(a);
-        }
-        for(int i = 1; i <= v; i++){
-            if(color[i] == 0){
-                dfs(i, 1);
-            }
+            g[a].push_back(b); g[b].push_back(a); 
         }
         bool ok = true;
-        for(int i = 1; i<= v; i++){
-            for(int j = 0; j<arr[i].size(); j++){
-                int k = arr[i][j];
-                if(color[i] == color[k]) ok = false;
+        for(int i = 1; i<=v; i++){
+            if(color[i]== 0){
+                if(bfs(i, 1) == false) ok = false;
             }
         }
-
-        if(ok) cout << "YES\n";
+        if(ok)cout << "YES\n";
         else cout << "NO\n";
     }
 }
