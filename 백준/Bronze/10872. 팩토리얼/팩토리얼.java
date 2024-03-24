31e12a87374f09071eaae489bd70589211d4e387
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-24 17:30:53 +0900

    [Bronze III] Title: 팩토리얼, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        int arr[] = new int[15];
         int n = 0;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr[0] = 1; arr[1] = 1; arr[2] = 2;
-
-        for(int i = 3; i<=n; i++){
-            arr[i] = arr[i-1] * i;
-        }
-        System.out.print(arr[n]);
+        int res = facto(n);
+        System.out.print(res);
+    }
+    public  static  int facto(int n){
+        if(n == 0) return 1;
+        else return facto(n-1) * n;
     }
 }
