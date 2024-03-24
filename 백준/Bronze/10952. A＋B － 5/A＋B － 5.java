dce82b25d717b5356ee6c75be46e4fa3819afad5
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-24 17:40:55 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static  void main(String[] args){
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            a = sc.nextInt(); b = sc.nextInt();
+            if(a == 0 && b == 0) break;
+            System.out.println(a+b);
+        }
+    }
+}
