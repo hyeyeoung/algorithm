f98d5d8e77ab15e594e2e874d139d7943be1af67
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-03-24 15:43:12 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,53 +1,36 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-
-int n;
-char arr[101][101];
-bool ok1[101][101];
-bool ok2[101][101];
-
+//////////////
 int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
+/////////////
+char arr[101][101] = {};
+bool ok1[101][101] = {};
+bool ok2[101][101] = {};
+int n;
 
 void go1(int x, int y, char a){
-    queue <pair<int,int>> q;
-    q.push({x,y});
     ok1[x][y] = true;
-    while(!q.empty()){
-        x = q.front().first, y = q.front().second;
-        q.pop();
-        for(int i = 0; i<4; i++){
-            int nx = x+dx[i], ny = y + dy[i];
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && !ok1[nx][ny]){
-                if(arr[nx][ny] == a){
-                    q.push({nx,ny});
-                    ok1[nx][ny] = true;
-                }
+    for(int i = 0; i<4; i++){
+        int nx = x + dx[i], ny = y+dy[i];
+        if(nx >= 0 && nx < n && ny>= 0 && ny<n && !ok1[nx][ny]){
+            if(arr[nx][ny] ==  a){
+                go1 (nx,ny, a);
             }
         }
     }
 }
+
 void go2(int x, int y, char a){
-    queue <pair<int,int>> q;
-    q.push({x,y});
     ok2[x][y] = true;
-    while(!q.empty()){
-        x = q.front().first, y = q.front().second;
-        q.pop();
-        for(int i = 0; i<4; i++){
-            int nx = x+dx[i], ny = y + dy[i];
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && !ok2[nx][ny]){
-                if(a == 'R' || a == 'G'){
-                    if(arr[nx][ny] == 'R' || arr[nx][ny] == 'G'){
-                        ok2[nx][ny] = true;
-                        q.push({nx,ny});
-                    }
-                }
-                else if(a=='B' && arr[nx][ny] == 'B') {
-                    ok2[nx][ny] = true;
-                    q.push({nx,ny});
-                }
+    for(int i = 0; i<4; i++){
+        int nx = x + dx[i], ny = y+dy[i];
+        if(nx >= 0 && nx < n && ny>= 0 && ny<n && !ok2[nx][ny]){
+            if(a == 'B' && arr[nx][ny] ==  a){
+                go2(nx,ny, a);
+            }
+            else if(a=='R'||a=='G'){
+                if(arr[nx][ny]=='R' || arr[nx][ny]=='G')   go2(nx,ny, a);
             }
         }
     }
@@ -55,24 +38,24 @@ void go2(int x, int y, char a){
 
 int main(){
     cin >> n;
-    
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++) cin >> arr[i][j];
     }
 
-    int rb_watch = 0, rgb_watch = 0;
+    int rgb_watch = 0, rb_watch = 0;
 
     for(int i = 0; i<n; i++){
-        for(int j =0 ; j <n; j++){
+        for(int j = 0; j<n; j++){
             if(!ok1[i][j]){
                 rgb_watch ++;
-                go1(i,j, arr[i][j]);
+                go1(i,j,arr[i][j]);
             }
             if(!ok2[i][j]){
-                rb_watch ++;
-                go2(i,j, arr[i][j]);
+                rb_watch++;
+                go2(i,j,arr[i][j]);
             }
         }
     }
-    cout << rgb_watch <<" "<< rb_watch;
+    cout << rgb_watch << " " << rb_watch;
+
 }
