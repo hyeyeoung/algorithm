89bd5e420a21c324099cab4576dddc6207a42d62
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-04-11 22:15:05 +0900

    [Gold V] Title: 직각삼각형, Time: 1900 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1711. 직각삼각형/직각삼각형.cc
+++ 백준/Gold/1711. 직각삼각형/직각삼각형.cc
@@ -6,7 +6,6 @@ int n;
 vector <pair<long long, long long>> v;
 
 int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     cin >> n;
     for(int i = 0; i<n; i++){
         int a, b; cin >> a >> b;
