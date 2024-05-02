1e7dc5f2fc3202f9b6b70a145505dcd725d83a96
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-05-02 18:24:53 +0900

    [Silver I] Title: IOIOI, Time: 212 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,17 +1,24 @@
-def CNT(S, n, m):
-    cnt = 0
-    str = 'IO'*n
-    str += 'I'
-    n = 2*n + 1
-    for i in range(m):
-        if i+n <= m and S[i:i+n] == str:
-            cnt+=1
-        
-    return cnt
+import sys
+input = sys.stdin.readline
 
+def CNT(S, n, m):
+    cnt = 0 # 종합개수
+    index = 0 # 현재위치
+    tmp = 0 # IOI의 개수
+    while index < m-2:
+        if S[index] == 'I' and S[index+1] == 'O' and S[index+2] == 'I':
+            tmp += 1
+            index += 2
+            if tmp == n:
+                cnt += 1
+                tmp -= 1
+        else:
+            tmp = 0
+            index+=1
+    return cnt
 
 if __name__=='__main__':
     n = int(input()) # P의 길이
     m = int(input()) # S의 길이
-    S = input() # S
-    print(CNT(S,n,m))
+    S = input().rstrip() # S
+    print(CNT(S, n, m))    
