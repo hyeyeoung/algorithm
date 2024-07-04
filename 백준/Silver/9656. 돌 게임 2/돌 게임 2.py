f53a5ab6090110388792b27b39363061fd0ec3bc
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-07-04 17:23:03 +0900

    [Silver V] Title: 돌 게임 2, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    # 1 또는 3
+    # 상근 -> 창영
+    # arr[1] -> 상근 arr[2] -> 창영 arr[3] -> 상근
+    # arr[4] ->
+    if n % 2 == 0: print("SK")
+    else: print("CY")
