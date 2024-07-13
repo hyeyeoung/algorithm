91c780f93c91a8885dc23ad37f4983fc5059381f
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-07-13 21:51:50 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    o = input()
+
+    if o == 'M': print('MatKor')
+    if o == 'W': print('WiCys')
+    if o == 'C': print('CyKor')
+    if o == 'A': print('AlKor')
+    if o == '$': print('$clear')
