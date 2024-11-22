eb7201c5f350cdd7520fec477b282ea0d5ebb181
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-11-22 23:20:57 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char arr[21];
+    cin >> arr;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << arr << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
