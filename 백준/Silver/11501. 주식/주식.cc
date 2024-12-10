46a749987e8ecfa24e6b024ffa607a9600b18fe0
hy <81437055+hyeyeoung@users.noreply.github.com>
2024-12-10 16:45:23 +0900

    [Silver II] Title: 주식, Time: 560 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int TC;
     cin >> TC;
     while(TC--){
@@ -12,16 +12,15 @@ int main(){
         vector <long long> arr(l);
         for(int i = 0; i<l; i++) 
             cin >> arr[i];
+
         long long cur = arr[l-1], total = 0;
-        
         int idx = l-1;
-        
+
         while(idx >= 0){
             if(cur < arr[idx]) 
                 cur = arr[idx];
-            else{
+            else
                 total += (cur - arr[idx]);
-            }
             idx--;
         }
         cout << total << '\n';
