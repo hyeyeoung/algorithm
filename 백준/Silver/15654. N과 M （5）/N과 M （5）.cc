36cccc93ba3f5d7dc9d15e3e4b9cde9b94981ef3
hy <81437055+hyeyeoung@users.noreply.github.com>
2025-02-03 16:34:35 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int arr[10]={0};
-int a[10] = {0};
-bool b[10] = {0};
-void go(int index, int n, int m){
-    if(index == m+1){
-        for(int i = 1; i<= m; i++) cout << a[i] <<" ";
-        cout <<'\n';
+// 재귀 풀이 : 시간이 많이 걸림.
+bool c[10];
+int arr[10]; 
+int a[10];
+int n, m;
+void go(int index)
+{
+    if(index == m+1)
+    {
+        for(int i = 1; i <= m; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << '\n';
         return;
     }
-    for(int i = 1; i<=n; i++){
-        if(b[i]) continue;
-        b[i] = true; a[index] = arr[i];
-        go(index+1, n, m);
-        b[i] = false;
+    for(int i = 1; i <= n; i++)
+    {
+        if(c[i]) continue;
+        c[i] = true;
+        a[index] = arr[i];
+        go(index+1);
+        c[i] = false;
     }
 }
-int main(){
-    int n, m; cin >> n >> m;
-    for(int i = 1; i<=n; i++)cin >> arr[i];
+int main()
+{
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> arr[i];
     sort(arr, arr+n+1);
-    go(1, n, m);
+
+    go(1);
 }
