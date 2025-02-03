c2eae6a5d8122798d950c8127615b4c42d6d4294
hy <81437055+hyeyeoung@users.noreply.github.com>
2025-02-03 16:46:03 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int arr[10]; int a[10];
-// void go(int index, int n, int m){
-//     if(index == m+1) {
-//         for(int i = 1; i<=m; i++) cout << arr[a[i]]<<" ";
-//         cout <<'\n';
-//         return;
-//     }
-//     for(int i = 1; i<=n; i++){
-//         a[index] = i;
-//         go(index+1, n, m);
-//     }
-// }
-void go(int index, int select, int n, int m){
-    if(select == m){
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j < a[i] ; j++){
-                cout << arr[i] << " ";
-            }
+// 재귀 풀이 : 시간이 많이 걸림.
+bool c[10];
+int arr[10]; 
+int a[10];
+int n, m;
+void go(int index, int start)
+{
+    if(index == m+1)
+    {
+        for(int i = 1; i <= m; i++)
+        {
+            cout << a[i] << " ";
         }
-        cout <<'\n';
+        cout << '\n';
         return;
     }
-    if(index >= n) return;
-    for(int i = m-select; i >= 1 ; i--){
-        a[index] = i;
-        go(index+1, select+i, n, m);
+    for(int i = start; i <= n; i++)
+    {
+        a[index] = arr[i];
+        go(index+1, i);
     }
-    a[index] = 0;
-    go(index+1, select, n, m);
 }
-int main(){
-    int n, m; cin >> n >> m;
-    for(int i = 0; i<n; i++) cin >> arr[i];
-    sort(arr, arr+n);
-    go(0, 0,n, m);
+int main()
+{
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+    sort(arr, arr+n+1);
+    go(1, 1);
 }
