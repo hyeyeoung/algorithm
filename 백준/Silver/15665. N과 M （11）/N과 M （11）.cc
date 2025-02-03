e491908be9063852f1eb66616d3382a78826427b
hy <81437055+hyeyeoung@users.noreply.github.com>
2025-02-03 16:58:04 +0900

    [Silver II] Title: N과 M (11), Time: 148 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int arr[10]; int a[10]; int b[10]={0};
+int arr[10]; int a[10]; int b[10] = {0};
 void go(int index, int n, int m){
-    if(index == m+1) {
-        for(int i =1; i<=m;i++) cout << arr[a[i]]<<" ";
-        cout << '\n';
+    if(index == m+1){
+        for(int i = 1; i<=m; i++) cout <<arr[a[i]]<<" ";
+        cout <<"\n";
         return;
     }
     for(int i = 1; i<=n; i++){
-        if(b[i]>0){
+        if(b[i] > 0){
+            // b[i]-=1;
             a[index] = i;
-            go(index+1, n ,m);
+            go(index+1, n, m);
+            // b[i]+=1;
         }
     }
 }
+
 int main(){
     int n, m; cin >> n >> m;
-    for(int i = 1; i<=n; i++) cin >> arr[i];
-    sort(arr,arr+n+1);
-
-    for(int i =1; i<=n;i+=b[i]){
+    for(int i = 1; i<=n;i++) cin >> arr[i];
+    sort(arr,arr+1+n);
+    
+    for(int i = 1; i<=n; i+=b[i]){
         int count = 1;
-        for(int j = i+1; j<=n;j++){
+        for(int j = i+1; j<=n; j++){
             if(arr[i] == arr[j]) count++;
             else break;
         }
