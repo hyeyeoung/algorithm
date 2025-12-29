70cf388a21680b5b125fa5adce057052fb2511e4
hy <81437055+hyeyeoung@users.noreply.github.com>
2025-12-30 00:12:46 +0900

    [Bronze V] Title: Serca, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+
+    while(a--){
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+}
